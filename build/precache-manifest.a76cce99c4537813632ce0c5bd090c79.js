94dd3f784dae11ce9077a009cc4642e1144cd6a5
Tan Nguyen <tanngocit@gmail.com>
2020-08-01 21:48:20 +0700

    Your commit message

---
+++ build/precache-manifest.a76cce99c4537813632ce0c5bd090c79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee2820fe7a36e98b62906db65370aa8c",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "589396c01d35cefd4da6",
+    "url": "/covid19-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "46431705497cda20b967",
+    "url": "/covid19-tracker/static/css/main.bd3875ab.chunk.css"
+  },
+  {
+    "revision": "589396c01d35cefd4da6",
+    "url": "/covid19-tracker/static/js/2.251e2ce8.chunk.js"
+  },
+  {
+    "revision": "f4fbb9c35749914b4c7d298d36ddb8c4",
+    "url": "/covid19-tracker/static/js/2.251e2ce8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46431705497cda20b967",
+    "url": "/covid19-tracker/static/js/main.341c385c.chunk.js"
+  },
+  {
+    "revision": "c5535707c668a65b0e80",
+    "url": "/covid19-tracker/static/js/runtime-main.c7a49258.js"
+  }
+]);
