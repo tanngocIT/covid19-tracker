46872f07b14cb0cd4e3e0280988ce7587e1d2b7c
Tan Nguyen <tanngocit@gmail.com>
2020-08-02 16:42:16 +0700

    Updates

--- precache-manifest.1da225f0512682675d650276b2e49e31.js
+++ precache-manifest.1da225f0512682675d650276b2e49e31.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f2ee6278cd15a2ed26db590b5705d877",
+    "revision": "4f7758f6ec4cd95b4b6a1bbbb5f323c5",
     "url": "/covid19-tracker/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/covid19-tracker/static/css/2.7c35e809.chunk.css"
   },
   {
-    "revision": "9887a67f2ef095843c23",
+    "revision": "0aa4d0669dcad63e6a43",
     "url": "/covid19-tracker/static/css/main.9180af2b.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/covid19-tracker/static/js/2.251e2ce8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9887a67f2ef095843c23",
-    "url": "/covid19-tracker/static/js/main.792dbcd0.chunk.js"
+    "revision": "0aa4d0669dcad63e6a43",
+    "url": "/covid19-tracker/static/js/main.02dbf693.chunk.js"
   },
   {
     "revision": "c5535707c668a65b0e80",
